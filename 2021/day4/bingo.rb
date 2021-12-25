7cda6788a1d9171cbe10344f3e9f8f7990c267ca
wenner <nelsondiasdemedeiros@gmail.com>
2021-12-25 15:05:44 -0300

    add part II

--- 2021/day4/bingo.rb
+++ 2021/day4/bingo.rb
@@ -11,17 +11,33 @@ class Bingo
       boards.each do |board|
         board.mark(n)
         
-        if board.bingo?
+        if board.bingo? && @winning_board.nil?
           @winning_board = board
           @last_number = n
-          return 
+        end
+
+        if @losing_board.nil? && losing_boards.size == 1
+          @losing_board = losing_boards.first
+        end
+
+        if !@losing_board.nil? && @losing_board.bingo?
+          @losing_number = n
+          return
         end
       end
     end
   end
 
+  def losing_score
+    @losing_board.score * @losing_number
+  end
+
   def score
     winning_board.score * @last_number
+  end
+
+  def losing_boards
+    boards.select { |b| !b.bingo? }
   end
 
   def winning_board
@@ -79,5 +95,6 @@ if __FILE__ == $0
 
   bingo = Bingo.new(boards, numbers)
   bingo.play
-  puts bingo.score
+  puts "Part I: #{bingo.score}"
+  puts "Part II: #{bingo.losing_score}"
 end
